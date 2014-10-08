d8fb863f215924e01de54a071efe1be782f908b6
Daiwei Lu <daiweilu123@gmail.com>
2014-10-08 01:16:05 -0400

    Pull data

--- app.rb
+++ app.rb
@@ -1,17 +1,28 @@
 require 'twitter'
 require 'multi_json'
 require 'oj'
+require 'csv'
 
 Oj.default_options = {mode: :compat}
 MultiJson.use Oj
 
 $client = Twitter::REST::Client.new do |config|
-  config.consumer_key    = 'KTJsDCW16m4FmzODN66apbM3s'
-  config.consumer_secret = 'Q0MYQEMRoRei7MtRf3CDAFeBy0nxKLIvzhVvHFjrB5YKHXi2UC'
+  config.consumer_key    = 'KaP6dQMgDQypv7fVvQ7Q'
+  config.consumer_secret = 'Spy6Ei4BVttpSJjYMTubto79xV9Sfo1H1E843xpP8'
 end
 
-tweet = Twitter::Tweet.new({id: 517181915472740352})
+targets = [517101582534852608, 517101577631694848, 517101563438178305,
+  517101561269723137, 517101546111533056, 517101437713932289]
 
-arr = $client.retweets tweet, "count" => 200
+targets.each do |id|
+  res = $client.retweets id, count: 100
 
-puts arr[arr.size - 1].attrs[:id] - 1 # max_id
+  CSV.open("./retweets_of_#{id}.csv", 'w') do |csv|
+    csv << res[0].to_h.map {|key, val| key.to_s}
+    res.each do |tweet|
+      csv << tweet.to_h.map {|key, val| val.class == Hash ? nil : val.to_s}
+    end
+  end
+end
+
+__END__
