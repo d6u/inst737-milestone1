cbe09add1d4df9b16c0ebc3f03a6cb68c55b3448
Daiwei Lu <daiweilu123@gmail.com>
2014-10-07 23:18:57 -0400

    Update sample

--- app.rb
+++ app.rb
@@ -5,20 +5,13 @@ require 'oj'
 Oj.default_options = {mode: :compat}
 MultiJson.use Oj
 
-FILE_NAME = 'WWDC14.csv'
-
 $client = Twitter::REST::Client.new do |config|
   config.consumer_key    = 'KTJsDCW16m4FmzODN66apbM3s'
   config.consumer_secret = 'Q0MYQEMRoRei7MtRf3CDAFeBy0nxKLIvzhVvHFjrB5YKHXi2UC'
 end
 
-def fetch_search()
-  count = 0
-  $client.search('#IceBucketChallenge until:2014-09-30', include_entities: true, result_type: 'recent').each do |tweet|
-    puts tweet.created_at
-    count += 1
-  end
-  puts '--> ', count
-end
+tweet = Twitter::Tweet.new({id: 517181915472740352})
 
-fetch_search()
+arr = $client.retweets tweet, "count" => 200
+
+puts arr[arr.size - 1].attrs[:id] - 1 # max_id
